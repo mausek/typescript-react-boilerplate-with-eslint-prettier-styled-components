01e23b574c7dea6ac717e4ae052da6a777ff23a3
Rok Stojnšek <stojnsek@gmail.com>
2020-12-19 20:19:27 +0100

    HelloWorld using Rebass Box and Text component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import { useTranslation } from 'react-i18next'
+import { Box, Text } from 'rebass'
 
 const App: React.FC = () => {
   const { t } = useTranslation()
   return (
-    <div>
-      {t('helloWorld')}
-    </div>  
+    <Box>
+      <Text>{t('helloWorld')}</Text>
+    </Box>  
   );
 }
 
